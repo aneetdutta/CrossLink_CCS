c03814e62fa9e435eb6eae44914334c516e8efec
aneetdutta <32698532+aneetdutta@users.noreply.github.com>
2026-04-26 20:11:05 +0200

    Update perfect_mixcount.py

--- analysis/perfect_mixcount.py
+++ analysis/perfect_mixcount.py
@@ -134,7 +134,7 @@ def interpolate_and_check(stats, user_data, base_threshold):
 
 # Main execution
 if __name__ == "__main__":
-    csv_file = "/home/aneet_wisec/usenix_2025/path-leakage/data/scenario_exponential_512_sumo_all/user_data_scenario_exponential_512_sumo_all.csv"
+    csv_file = "/path-leakage/data/scenario_exponential_512_sumo_all/user_data_scenario_exponential_512_sumo_all.csv"
     base_threshold = 3
     Vmax = 3
 
