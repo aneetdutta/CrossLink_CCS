5a6c113c11a908df41e19e91b99d3461dabc6d75
aneetdutta <32698532+aneetdutta@users.noreply.github.com>
2026-05-03 00:34:55 +0200

    Update q4_density_graph.py

--- plot/q4_density_graph.py
+++ plot/q4_density_graph.py
@@ -18,8 +18,8 @@ def compute_ecdf(data):
     return x, ecdf
 
 # Path to directory containing CSV files (update as needed)
-csv_directory_mix = '/home/aneet_wisec/usenix_2025/path-leakage/plot/q4_mix'
-csv_directory_score = '/home/aneet_wisec/usenix_2025/path-leakage/plot/q4_density_data'
+csv_directory_mix = '/path-leakage/plot/q4_mix'
+csv_directory_score = '/path-leakage/plot/q4_density_data'
 # Pattern to match CSV files
 csv_pattern = os.path.join(csv_directory_score, '*.csv')
 
